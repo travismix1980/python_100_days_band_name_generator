d70a66c0a293435d339cf5c7f08fcf762521d74b
Travis A Mix <36829787+travismix1980@users.noreply.github.com>
2021-10-19 00:36:58 +0000

    We now get the city the user grew up in

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ print("Having problems figuring out a name for your band?")
 print("You have come to the right place :)")
 
 #2. Ask the user for the city that they grew up in.
+grew_up = input("In what city did you grow up? ")
 
 #3. Ask the user for the name of a pet.
+
 
 #4. Combine the name of their city and pet and show them their band name.
 
