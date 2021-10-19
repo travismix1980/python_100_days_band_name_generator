542a0164666b0822ee569c8c9eb923be03eb2269
Travis A Mix <36829787+travismix1980@users.noreply.github.com>
2021-10-19 00:49:50 +0000

    We now combine both inputs into a band name. Day 1 Project Complete

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ print("Having problems figuring out a name for your band?")
 print("You have come to the right place :)")
 
 #2. Ask the user for the city that they grew up in.
-grew_up = input("In what city did you grow up? ")
+grew_up = input("In what city did you grow up? \n")
 
 #3. Ask the user for the name of a pet.
-pet = input("What was the name of your first pet? ")
+pet = input("What was the name of your first pet? \n")
 
 #4. Combine the name of their city and pet and show them their band name.
+print("Your band name is: " + grew_up + " " + pet)
 
 #5. Make sure the input cursor shows on a new line, see the example at:
 #   https://band-name-generator-end.appbrewery.repl.run/
